96c6ed3c1996ef61031a6fb93a9c83a4b7cbb671
hamza-ali-565 <alimuhammadhamza402@gmail.com>
2024-08-02 20:31:17 +0500

    test creation posting completed

--- src/controllers/LABS/test.controller.mjs
+++ src/controllers/LABS/test.controller.mjs
@@ -1,34 +1,59 @@
 import { ApiError } from "../../utils/ApiError.mjs";
 import { asyncHandler } from "../../utils/asyncHandler.mjs";
 import { ApiResponse } from "../../utils/ApiResponse.mjs";
+import { labTestModel } from "../../models/LAB.Models/test.model.mjs";
 
 const labTest = asyncHandler(async (req, res) => {
-  const { testname, metaData } = req.body;
+  const {
+    testName,
+    department,
+    category,
+    testType,
+    reportDays,
+    active,
+    style,
+    testRanges,
+  } = req.body;
 
-  if (!testname || !metaData) {
+  if (
+    ![
+      testName,
+      department,
+      category,
+      testType,
+      reportDays,
+      active,
+      style,
+    ].every(Boolean)
+  ) {
     throw new ApiError(400, "All fields are required");
   }
 
-  // const user = await User.findByIdAndUpdate(
-  //     req.user?._id,
-  //     {
-  //         $set: {
-  //             fullName,
-  //             email: email
-  //         }
-  //     },
-  //     {new: true}
+let rangeInfo = []
+  if (testRanges.length > 0){
+rangeInfo = testRanges.filter((items)=> items.equipment !== "")
+  }
 
-  // ).select("-password")
+  console.log("ranges", rangeInfo);
+  
+  const response = await labTestModel.create({
+    testName,
+    department,
+    category,
+    testType,
+    reportDays,
+    active,
+    style,
+    testRanges: rangeInfo,
+    thisIs: "Test",
+    createdUser: req?.user?.userId,
+  });
+console.log('response of lab test ', response);
 
   return res
     .status(200)
     .json(
-      new ApiResponse(
-        200,
-        { user: "hamza" },
-        "Account details updated successfully"
-      )
+      new ApiResponse(200, { data: response }, "TEST CREATED SUCCESSFULLY !!!")
     );
 });
 
---
+++ src/models/LAB.Models/test.model.mjs
@@ -0,0 +1,26 @@
+import mongoose, { Schema, model } from "mongoose";
+import { getCreatedOn } from "../../constants.mjs";
+
+import AutoIncrementFactory from "mongoose-sequence";
+
+const AutoIncrement = AutoIncrementFactory(mongoose.connection);
+
+const labTest = new Schema({
+  testCode: { type: Number, unique: true },
+  testName: { type: String, required: true, uppercase: true },
+  department: { type: String, required: true },
+  category: { type: String, required: true },
+  testType: { type: String, required: true },
+  reportDays: { type: String, required: true },
+  active: { type: Boolean, required: true },
+  style: { type: Array, required: true }, // array
+  testRanges: { type: Array }, //array
+  thisIs: { type: String, required: true },
+  createdUser: { type: String, required: true },
+  createdOn: { type: String, default: getCreatedOn() },
+  // groupInfo: {},
+});
+
+labTest.plugin(AutoIncrement, { inc_field: "testCode" });
+
+export const labTestModel = model("Lab Test", labTest);
